3405d8398fc84b8e0b8bb80ab2ffed30612f96c5
umme <unnipa01@yahoo.com>
2021-07-27 21:10:13 -0500

    set01 changed

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,44 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
+            //throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
+            //throw new NotImplementedException();
+
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+                return number1;
+            else
+                return number2;
+
+            //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+            
+            //throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return "Hello!";
+            //throw new NotImplementedException();
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
+            //throw new NotImplementedException();
         }
     }
 }
